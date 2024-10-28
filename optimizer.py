14b11440cd9a3593c3d4e6da204abfdfb37f2a0a
Deceptrax123 <srinitishsri@gmail.com>
2024-10-29 00:14:01 +0530

    add hebbian optimizer script

--- optimizer.py
+++ optimizer.py
@@ -10,5 +10,10 @@ class HebbianDescent(Optimizer):
 
         self.state = dict()
 
-    def step():
-        pass
+    def step(self, x, leaf_grad):
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                p.data -= group['lr'] * \
+                    torch.matmul(torch.add(x, -torch.mean(x)), leaf_grad)
